6a5d4d742a09480908f1a33fdcae100fbbb34511
Tom5521 <88908582+Tom5521@users.noreply.github.com>
2023-03-12 16:43:56 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Tom5521",
     author_email="mismarttv321@gmail.com",
-    url="https://github.com/Tom5521/PY-pacman",
+    url="https://github.com/Tom5521/PY-PackArch",
     py_modules=["packarch"],
     keywords=["pacman", "arch linux"],
     download_url="https://github.com/Tom5521/PY-pacman/archive/refs/tags/1.5.4.tar.gz",
