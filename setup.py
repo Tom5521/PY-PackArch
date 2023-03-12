8ee36e7e7b4bdce527353e0d1c43571d672d3bb5
Tom5521 <88908582+Tom5521@users.noreply.github.com>
2023-03-12 16:44:58 -0300

    Update Setup

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url="https://github.com/Tom5521/PY-PackArch",
     py_modules=["packarch"],
     keywords=["pacman", "arch linux"],
-    download_url="https://github.com/Tom5521/PY-pacman/archive/refs/tags/1.5.4.tar.gz",
+    download_url="https://github.com/Tom5521/PY-pacman/archive/refs/tags/1.8.3.tar.gz",
     license="GPL-3.0",
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
